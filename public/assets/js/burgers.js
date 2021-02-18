f06ca5cff740f120fd9213840e8643db1ce7d01a
sloanlacey <sloanlacey89@gmail.com>
2021-02-17 17:01:42 -0800

    Create handlebars files, build JS handle click events on buttons and rendering new burgers, as well as updating existing burgers to new container.

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,44 @@
+$(function() {
+    $(".change-status").on("click", function(event) {
+        const id = $(this).data("id");
+
+        const burgerStatus = {
+            devoured: true
+        };
+
+        console.log(burgerStatus);
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: burgerStatus
+        }).then(
+            function() {
+                console.log("Changed burger status to ", burgerStatus.devoured);
+                location.reload();
+            }
+        )
+    })
+
+    $(".newBurgerForm").on("submit", function(event) {
+        event.preventDefault();
+
+        $(".burgersContainer").show();
+
+        const newBurger = {
+            burger_name: $("#newBurgerName").val().trim(),
+            devoured: 0
+        }
+
+        console.log(newBurger);
+
+        $.ajax("/api/burgers/", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("Created new burger");
+                location.reload();
+            }
+        )
+    })
+});
