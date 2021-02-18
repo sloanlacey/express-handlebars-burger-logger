6f506d21470e5add4696fb9254e998991dbe9fa4
sloanlacey <sloanlacey89@gmail.com>
2021-02-17 16:49:47 -0800

    Create schema and seed files to organize the MySQL DB.

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('hamburger', false),
+('cheeseburger', false),
+('bacon cheeseburger', false),
+('mushroom and swiss burger', false),
+('veggie burger', false),
+('beyond burger', false),
+('patty melt', false),
+('butter burger', false),
+('smash burger', false),
+('fried onion burger', false),
+('steam burger', false);
